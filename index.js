a4e657118e04ab1587f8faa2bf4247b31fb99a9e
mathkaynsgvsjbhiabhsbu <kayodematthew807@gmail.com>
2026-04-28 20:39:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+// Force delete corrupted WhatsApp session on startup
+const authPath = path.join(__dirname, 'auth_info_baileys');
+if (fs.existsSync(authPath)) {
+  fs.rmSync(authPath, { recursive: true, force: true });
+  console.log('Deleted corrupted auth_info_baileys folder');
+}
 const { default: makeWASocket, useMultiFileAuthState, fetchLatestBaileysVersion } = require('@whiskeysockets/baileys');
 
 async function startBot() {
-  const { state, saveCreds } = await useMultiFileAuthState('auth');
+  const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys');
   const { version } = await fetchLatestBaileysVersion();
   
   const sock = makeWASocket({ 
